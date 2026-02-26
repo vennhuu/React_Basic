04a9e5c5b626a9130027f6928357290319a4d6ef
vennhuu <phuochuu0512@gmail.com>
2026-02-27 02:12:07 +0700

    mrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,36 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import {
+createBrowserRouter,
+RouterProvider,
+} from "react-router-dom";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/login",
+    element: <div> Login Page</div>
+  },
+  {
+    path: "/register",
+    element: <div> Register Page</div>
+  },
+  {
+    path: "/users",
+    element: <div> Users Page</div>
+  },
+  {
+    path: "/product",
+    element: <div> Products Page</div>
+  }
+]);
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </StrictMode>,
 )
