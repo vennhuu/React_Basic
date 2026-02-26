0b078b9d5282a3e162c9976d312ee86adf2f7917
vennhuu <phuochuu0512@gmail.com>
2026-02-26 17:41:50 +0700

    jsx

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,6 +1,14 @@
+import './style.css'
+
 const MyComponent = () => {
   return (
-    <div>Phan Hữu Phước</div>
+    // fragment : dùng nhiều div trong return 
+    <> 
+        <div >Phan Hữu Phước</div>
+        <div className="child"
+            style={{borderRadius: "10px"}}
+        >Child</div>
+    </>
   ) ;
 }
 export default MyComponent;
