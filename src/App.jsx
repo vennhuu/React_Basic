d78151eb3e060cb327c507d9881b6a8a4a4d0112
vennhuu <phuochuu0512@gmail.com>
2026-02-26 16:57:28 +0700

    arrow function

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0)
 
+  // function myFunction() {
+  //   console.log("Run my function") ;
+  // }
+
+  const myFunction = () => {
+    console.log("Run run")
+  }
+
+  myFunction() ;
   return (
     <>
       <div>
