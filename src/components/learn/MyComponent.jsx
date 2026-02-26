11ad46c49ed463ee877f054b7e2d366d191da4ac
vennhuu <phuochuu0512@gmail.com>
2026-02-26 17:48:17 +0700

    variable jsx

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,10 +1,22 @@
 import './style.css'
 
+// {} dùng để code js
 const MyComponent = () => {
+
+    // const venn = "eric"; //string
+    // const venn = 25; //number
+    // const venn = true; //boolean
+    // const venn = undefined;
+    // const venn = null;
+    // const venn = [1 , 2 , 3]; //array
+    const venn = {
+        name: "Venn" , 
+        age : 29
+    }; //object
   return (
     // fragment : dùng nhiều div trong return 
     <> 
-        <div >Phan Hữu Phước</div>
+        <div> {JSON.stringify(venn)} Phan Hữu Phước</div>
         <div className="child"
             style={{borderRadius: "10px"}}
         >Child</div>
