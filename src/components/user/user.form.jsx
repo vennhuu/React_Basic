100007cec5103d778733becfb064b07290462013
vennhuu <phuochuu0512@gmail.com>
2026-02-27 13:36:35 +0700

    form

---
+++ src/components/user/user.form.jsx
@@ -0,0 +1,52 @@
+import { Button, Input } from "antd";
+import { useState } from "react";
+
+const UserForm = () => {
+    
+    const [fullName , setFullName] = useState("") ;
+    const [email , setEmail] = useState("") ;
+    const [phoneNumber , setPhoneNumber] = useState("") ;
+    const [password , setPassword] = useState("") ;
+
+    const handleClickBtn = () => {
+        console.log("Đã lưu" , {fullName , email , phoneNumber , password});
+    }
+    return (
+        <div className="user-form" style={{margin: "20px 0"}}>
+            <div style={{ display: "flex", gap: "15px", flexDirection: "column"}}>
+                <div>
+                    <span>Full name</span>
+                    <Input 
+                        value={fullName}
+                        onChange={(event) => setFullName(event.target.value)}
+                    />
+                </div>
+                <div>
+                    <span>Email</span>
+                    <Input
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                    />
+                </div>
+                <div>
+                    <span>Phone number</span>
+                    <Input
+                        value={phoneNumber}
+                        onChange={(event) => setPhoneNumber(event.target.value)}
+                    />
+                </div>
+                <div>
+                    <span>Password</span>
+                    <Input.Password
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                    />
+                </div>
+                <div>
+                    <Button type="primary" onClick={handleClickBtn}>Create a new user</Button>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default UserForm;
--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -1,7 +1,13 @@
+import UserForm from "../components/user/user.form";
+import UserTable from "../components/user/user.table";
 
 const UserPage = () => {
     return (
-        <div>User Page</div>
+    
+        <div style={{padding: "20px"}}>
+            <UserForm/>
+            <UserTable/>
+        </div>
     )
 }
 
